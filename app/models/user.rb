a73e2956a94ccd88f7a4c777b6949fe5a7d35311
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-11 06:58:20 +0000

    userモデルにvalidates設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true ,length: {maximum: 30}
+  validates :email, presence: true ,length: {minimum: 255},
+                    format: { with: /\A[\W+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+                    
 end
