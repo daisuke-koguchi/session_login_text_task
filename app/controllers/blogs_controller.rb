c9da5fc3d8a548d4188028f9130d8f01a3b7c947
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-12 06:35:47 +0000

    パーシャル追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,5 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:show, :edit, :update, :destroy]
   def index
     @blog = Blog.all
   end
@@ -16,16 +17,13 @@ class BlogsController < ApplicationController
     end
   end
 
-  def show
-     @blog = Blog.find(params[:id])  
+  def show  
   end
   
   def edit
-    @blog = Blog.find(params[:id])
   end
 
   def update
-    @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
       redirect_to blogs_path, notice: "編集しました"
     else
@@ -34,13 +32,14 @@ class BlogsController < ApplicationController
   end
 
   def destroy
-    @blog = Blog.find(params[:id])
-    @blog.destroy
     redirect_to blogs_path, notice: "削除しました"
   end
 
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
+  end
+  def set_blog
+    @blog = Blog.find(params[:id])
   end
 end
