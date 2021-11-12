9aa91e90007d4a4ad670bcf94f8c243db766638d
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-12 03:19:33 +0000

    blogs機能のnew/createアクション追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :login_required, only: [:new,:create]
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
@@ -12,8 +13,10 @@ class SessionsController < ApplicationController
       render :new
     end
   end
+
   def destroy
     session.delete(:user_id)
     redirect_to new_session_path
   end 
+  
 end
