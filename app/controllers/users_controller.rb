f8c27b9e33f255986d8271834b227c4a74c4ff36
Daisuke-Koguchi <ds05080906@gmail.com>
2021-11-12 01:52:10 +0000

    Showアクションでエラーが出ていたため修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,10 +13,13 @@ class UsersController < ApplicationController
       #失敗
       render :new
     end
+  end
+
   def show
     @user = User.find(params[:id])
+    binding.pry
   end
-  end
+
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, 
